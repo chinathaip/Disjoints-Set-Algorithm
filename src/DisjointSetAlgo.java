0015fd444dce5bad69bc8e605fc50ea5c32006ea
chinathaip <cartoonabe@gmail.com>
2022-02-22 17:00:28 +0700

    Initial commit

---
+++ src/DisjointSetAlgo.java
@@ -0,0 +1,12 @@
+public class DisjointSetAlgo {
+    public int[] rootArray;
+    public DisjointSetAlgo(int size){
+        for(int i=0;i<size;i++){
+            rootArray[i]=i;
+        }
+    }
+
+    public int find(int vertex){ //find root of vertex
+
+    }
+}
