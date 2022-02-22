077fc58425cf508a9630a469a05e11517e9eb53b
chinathaip <cartoonabe@gmail.com>
2022-02-22 17:19:51 +0700

    Added find, union, and connected function.

--- src/DisjointSetAlgo.java
+++ src/DisjointSetAlgo.java
@@ -7,6 +7,28 @@ public class DisjointSetAlgo {
     }
 
     public int find(int vertex){ //find root of vertex
+        return rootArray[vertex];
+    }
+    public void union(int vertex1, int vertex2){ //connect vertex2 to vertex1
 
+        //find root of each vertex
+        int RootOfVer1 = find(vertex1);
+        int RootOfVer2 = find(vertex2);
+
+        //check if they're already connected or not
+        //if they share the same root vertex, that means they're connected
+        if(RootOfVer1!=RootOfVer2){ //if they're different, they're not connected
+            //traverse the whole array change the root node
+            for(int i =0;i<rootArray.length;i++){
+                if(rootArray[i]==rootArray[RootOfVer2]){ //if vertex i is connected to vertex2
+                    //change its root to the root of vertex1
+                    rootArray[i]=RootOfVer1;
+                }
+            }
+        }
+    }
+
+    public boolean connected(int vertex1, int vertex2){
+        return find(vertex1)==find(vertex2);
     }
 }
