ec8abbd7dcbeb9cd1bcb9f072f3df8ad35f99bfb
chinathaip <cartoonabe@gmail.com>
2022-03-22 23:00:53 +0700

    more java
    add assigments + dota2 game

---
+++ src/Dota2Game/src/Dota2App.java
@@ -0,0 +1,310 @@
+import java.util.*;
+
+public class Dota2App {
+    public static void main (String[] args){
+        boolean GG = false;
+        char turn;
+        Scanner reader = new Scanner(System.in);
+        Set <Integer> checkDuplicate= new HashSet<>();
+        ArrayList <Hero> teamA = new ArrayList<>();
+        ArrayList <Hero> teamB = new ArrayList<>();
+        int userinput=0,userinput2=0,userinput3 = 0,userinput4 = 0,userinput5 = 0,userinput6 = 0;
+        //this line to line 45 is for user to choose the heroes
+        while(checkDuplicate.size()!=6) {
+            System.out.println("PlayerA, choose 3 heroes (1-10) *no duplicates*");
+            userinput = reader.nextInt();
+            userinput2 = reader.nextInt();
+            userinput3 = reader.nextInt();
+            System.out.println("PlayerB, choose 3 heroes (1-10) *no duplicates*");
+            userinput4 = reader.nextInt();
+            userinput5 = reader.nextInt();
+            userinput6 = reader.nextInt();
+            checkDuplicate.add(userinput);
+            checkDuplicate.add(userinput2);
+            checkDuplicate.add(userinput3);
+            checkDuplicate.add(userinput4);
+            checkDuplicate.add(userinput5);
+            checkDuplicate.add(userinput6);
+            if(checkDuplicate.size()!=6){
+                System.out.println("Duplicates detected, enter again");
+                checkDuplicate.clear();
+            }
+        }
+        Hero P1_hero1 = new Hero(userinput);
+        Hero P1_hero2 = new Hero(userinput2);
+        Hero P1_hero3 = new Hero(userinput3);
+        teamA.add(P1_hero1);
+        teamA.add(P1_hero2);
+        teamA.add(P1_hero3);
+
+        Hero P2_hero1 = new Hero(userinput4);
+        Hero P2_hero2 = new Hero(userinput5);
+        Hero P2_hero3 = new Hero(userinput6);
+        teamB.add(P2_hero1);
+        teamB.add(P2_hero2);
+        teamB.add(P2_hero3);
+
+//        **THIS IS THE MAKEUP DATA**
+//        Hero P1_hero1 = new Hero(1);
+//        Hero P1_hero2 = new Hero(2);
+//        Hero P1_hero3 = new Hero(3);
+//        teamA.add(P1_hero1);
+//        teamA.add(P1_hero2);
+//        teamA.add(P1_hero3);
+//        Hero P2_hero1 = new Hero(4);
+//        Hero P2_hero2 = new Hero(5);
+//        Hero P2_hero3 = new Hero(6);
+//        teamB.add(P2_hero1);
+//        teamB.add(P2_hero2);
+//        teamB.add(P2_hero3);
+
+
+
+        Random rn = new Random();
+        int randomnum = rn.nextInt()%2;
+        if(randomnum==0){
+            turn = 'A';
+        }else{
+            turn = 'B';
+        }
+        while (GG!=true){
+            System.out.println("------------------------------------------------------------------------------");
+            printGame(teamA,teamB);
+            System.out.println("------------------------------------------------------------------------------");
+            System.out.println("The command format is: Hero number, Action, Hero number");
+            System.out.println("Player "+turn+", What is your move?:");
+            int chosen_hero = reader.nextInt();
+            char chosen_action = reader.next().charAt(0);
+            int chosen_target = reader.nextInt();
+            if(turn =='A'){
+                switch (chosen_hero) {
+                    case 1 -> {
+                        if(P1_hero1.isAlive()){
+                            switch (chosen_action) {
+                                case 'A' -> { //normal attack
+                                    P1_hero1.attack();
+                                    switch (chosen_target) {
+                                        case 1 -> P2_hero1.getAttackedBy(P1_hero1);
+                                        case 2 -> P2_hero2.getAttackedBy(P1_hero1);
+                                        case 3 -> P2_hero3.getAttackedBy(P1_hero1);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                case 'U' -> { //ultimate
+                                    P1_hero1.ultimate();
+                                    switch (chosen_target) {
+                                        case 1 -> P2_hero1.ultimatedby(P1_hero1);
+                                        case 2 -> P2_hero2.ultimatedby(P1_hero1);
+                                        case 3 -> P2_hero3.ultimatedby(P1_hero1);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                default -> System.out.println("Not recognised");
+                            }
+                            turn = 'B';
+                        }else{
+                            System.out.println("The chosen hero is ded lmao :3 skipping turn..");
+                            turn = 'B';
+                        }
+                    }
+                    case 2 -> {
+                        if(P1_hero2.isAlive()){
+                            switch (chosen_action) {
+                                case 'A' -> {
+                                    P1_hero2.attack();
+                                    switch (chosen_target) {
+                                        case 1 -> P2_hero1.getAttackedBy(P1_hero2);
+                                        case 2 -> P2_hero2.getAttackedBy(P1_hero2);
+                                        case 3 -> P2_hero3.getAttackedBy(P1_hero2);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                case 'U' -> {
+                                    P1_hero2.ultimate();
+                                    switch (chosen_target) {
+                                        case 1 -> P2_hero1.ultimatedby(P1_hero2);
+                                        case 2 -> P2_hero2.ultimatedby(P1_hero2);
+                                        case 3 -> P2_hero3.ultimatedby(P1_hero2);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                default -> System.out.println("Not recognised");
+                            }
+                            turn = 'B';
+                        }else{
+                            System.out.println("The chosen hero is ded lmao :3 skipping turn..");
+                            turn = 'B';
+                        }
+                    }
+                    case 3 -> {
+                        if(P1_hero3.isAlive()){
+                            switch (chosen_action) {
+                                case 'A' -> {
+                                    P1_hero3.attack();
+                                    switch (chosen_target) {
+                                        case 1 -> P2_hero1.getAttackedBy(P1_hero3);
+                                        case 2 -> P2_hero2.getAttackedBy(P1_hero3);
+                                        case 3 -> P2_hero3.getAttackedBy(P1_hero3);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                case 'U' -> {
+                                    P1_hero3.ultimate();
+                                    switch (chosen_target) {
+                                        case 1 -> P2_hero1.ultimatedby(P1_hero3);
+                                        case 2 -> P2_hero2.ultimatedby(P1_hero3);
+                                        case 3 -> P2_hero3.ultimatedby(P1_hero3);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                default -> System.out.println("Not recognised");
+                            }
+                            turn = 'B';
+                        }else{
+                            System.out.println("The chosen hero is ded lmao :3 skipping turn..");
+                            turn = 'B';
+                        }
+                    }
+                    default -> System.out.println("Not recognised");
+                }
+            }
+            else{ // turn B
+                switch (chosen_hero) {
+                    case 1 -> {
+                        if(P2_hero1.isAlive()){
+                            switch (chosen_action) {
+                                case 'A' -> {
+                                    P2_hero1.attack();
+                                    switch (chosen_target) {
+                                        case 1 -> P1_hero1.getAttackedBy(P2_hero1);
+                                        case 2 -> P1_hero2.getAttackedBy(P2_hero1);
+                                        case 3 -> P1_hero3.getAttackedBy(P2_hero1);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                case 'U' -> {
+                                    P2_hero1.ultimate();
+                                    switch (chosen_target) {
+                                        case 1 -> P1_hero1.ultimatedby(P2_hero1);
+                                        case 2 -> P1_hero2.ultimatedby(P2_hero1);
+                                        case 3 -> P1_hero3.ultimatedby(P2_hero1);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                default -> System.out.println("Not recognised");
+                            }
+                            turn = 'A';
+                        }else{
+                            System.out.println("The chosen hero is ded lmao :3 skipping turn..");
+                            turn = 'A';
+                        }
+
+                    }
+                    case 2 -> {
+                        if(P2_hero2.isAlive()){
+                            switch (chosen_action) {
+                                case 'A' -> {
+                                    P2_hero2.attack();
+                                    switch (chosen_target) {
+                                        case 1 -> P1_hero1.getAttackedBy(P2_hero2);
+                                        case 2 -> P1_hero2.getAttackedBy(P2_hero2);
+                                        case 3 -> P1_hero3.getAttackedBy(P2_hero2);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                case 'U' -> {
+                                    P2_hero2.ultimate();
+                                    switch (chosen_target) {
+                                        case 1 -> P1_hero1.ultimatedby(P2_hero2);
+                                        case 2 -> P1_hero2.ultimatedby(P2_hero2);
+                                        case 3 -> P1_hero3.ultimatedby(P2_hero2);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                default -> System.out.println("Not recognised");
+                            }
+                            turn = 'A';
+                        }else{
+                            System.out.println("The chosen hero is ded lmao :3 skipping turn..");
+                            turn = 'A';
+                        }
+
+                    }
+                    case 3 -> {
+                        if(P2_hero3.isAlive()){
+                            switch (chosen_action) {
+                                case 'A' -> {
+                                    P2_hero3.attack();
+                                    switch (chosen_target) {
+                                        case 1 -> P1_hero1.getAttackedBy(P2_hero3);
+                                        case 2 -> P1_hero2.getAttackedBy(P2_hero3);
+                                        case 3 -> P1_hero3.getAttackedBy(P2_hero3);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                case 'U' -> {
+                                    P2_hero3.ultimate();
+                                    switch (chosen_target) {
+                                        case 1 -> P1_hero1.ultimatedby(P2_hero3);
+                                        case 2 -> P1_hero2.ultimatedby(P2_hero3);
+                                        case 3 -> P1_hero3.ultimatedby(P2_hero3);
+                                        default -> System.out.println("Not recognised");
+                                    }
+                                }
+                                default -> System.out.println("Not recognised");
+                            }
+                            turn = 'A';
+                        }else{
+                            System.out.println("The chosen hero is ded lmao :3 skipping turn..");
+                            turn = 'A';
+                        }
+
+                    }
+                    default -> System.out.println("Not recognised");
+                }
+
+            }
+
+
+            if(alldead(teamA)){
+                System.out.println("PlayerB has won this game :)))");
+                GG = true;
+            }else if(alldead(teamB)){
+                System.out.println("PlayerA has won this game :3333");
+                GG = true;
+            }else{
+                GG = false;
+            }
+        }
+
+
+    }
+
+    public static void printGame(ArrayList<Hero>teamA,ArrayList<Hero>teamB){
+        int counter =1;
+        System.out.println("PlayerA\t\t\t\t\t\t\t\t\t PlayerB");
+        System.out.printf("%-5s %5s %5s %5s\t\t\t\t\t%-5s %5s %5s %5s \n","Name","Dmg","HP","MP"," Name","Dmg","HP","MP");
+        for(int i=0;i<3;i++){
+            System.out.printf("%d.%-5s %5s %5s %5s\t\t\t\t %d.%-5s %5s %5s %5s \n",counter,teamA.get(i).getHeroname(),(int)teamA.get(i).getAttackDamage(),(int)teamA.get(i).getCurrentHP(),(int)teamA.get(i).getCurrentMP(),counter
+                    ,teamB.get(i).getHeroname(),(int)teamB.get(i).getAttackDamage(),(int)teamB.get(i).getCurrentHP(),(int)teamB.get(i).getCurrentMP());
+            counter++;
+        }
+
+    }
+
+    public static boolean alldead (ArrayList<Hero>team){
+        int counter =0;
+        for(Hero hero:team){
+            if(!hero.isAlive()){
+                counter++;  // +1 for every single death
+            }
+        }
+        if (counter ==3){ // +3 means every hero died
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+}
---
+++ src/Dota2Game/src/Hero.java
@@ -0,0 +1,214 @@
+public class Hero {
+    //missing current HP, current Mana
+    String Heroname,AttackMsg;
+    float HP,currentHP,MP,currentMP,AttackDamage,Armor,UltimateCost,UltimateDamge;
+
+
+    //constructor
+    public Hero (int Heronum){
+        switch(Heronum){
+            case 1: //axe
+                Heroname="Axe";
+                AttackMsg="Axe hits...";
+                HP = 1000;
+                currentHP =HP;
+                MP = 600;
+                currentMP = MP;
+                AttackDamage = 55;
+                Armor = 2;
+                UltimateCost = 200;
+                UltimateDamge = 300;
+                break;
+            case 2: //slark
+                Heroname="Slark";
+                AttackMsg="zeeeeeee Slarked!";
+                HP = 800;
+                MP = 650;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 68;
+                Armor = -1;
+                UltimateCost = 300;
+                UltimateDamge = 400;
+                break;
+            case 3: //Lion
+                Heroname="Lion";
+                AttackMsg="Lion Roaring...";
+                HP = 1200 ;
+                MP = 400;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 70;
+                Armor = -2;
+                UltimateCost = 300;
+                UltimateDamge = 500;
+                break;
+            case 4: //LifeStealer
+                Heroname="LifeStealer";
+                AttackMsg="LS Tagh tagh!";
+                HP = 1100 ;
+                MP = 300;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 50;
+                Armor = 3;
+                UltimateCost = 100;
+                UltimateDamge = 300;
+                break;
+            case 5: //Beastmaster
+                Heroname = "Beastmaster";
+                AttackMsg = "Beatmaster shooooweah";
+                HP = 800 ;
+                MP = 600;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 60;
+                Armor = 0;
+                UltimateCost = 200;
+                UltimateDamge = 300;
+                break;
+            case 6: //Bristleback
+                Heroname = "Bristleback";
+                AttackMsg = "BB is coming...";
+                HP = 1300 ;
+                MP = 200;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 48;
+                Armor = 4;
+                UltimateCost = 150;
+                UltimateDamge = 800;
+                break;
+            case 7: //Tiny
+                Heroname = "Tiny";
+                AttackMsg = "Tiny punch!";
+                HP = 1100 ;
+                MP = 400;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 55;
+                Armor = 4;
+                UltimateCost = 130;
+                UltimateDamge = 300;
+                break;
+            case 8: //Phoenix
+                Heroname = "Phoenix";
+                AttackMsg = "Hoooooooooowa Phoenix";
+                HP = 800 ;
+                MP = 800;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 75;
+                Armor = 0;
+                UltimateCost = 250;
+                UltimateDamge = 350;
+                break;
+            case 9: //Medusa
+                Heroname = "Medusa";
+                AttackMsg = "Die by Medusa!";
+                HP = 1000 ;
+                MP = 700;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 50;
+                Armor = 1;
+                UltimateCost = 350;
+                UltimateDamge = 400;
+                break;
+            case 10: //Mirana
+                Heroname = "Mirana";
+                AttackMsg = "Mirana! Get my mighty arrow.";
+                HP = 900 ;
+                MP = 800;
+                currentHP =HP;
+                currentMP = MP;
+                AttackDamage = 55;
+                Armor = 1;
+                UltimateCost = 270;
+                UltimateDamge = 320;
+                break;
+            default:
+                Heroname ="XD";
+        }
+
+    }
+
+    public String getHeroname(){
+        return Heroname;
+    }
+
+    public String getAttackMsg(){
+        return AttackMsg;
+    }
+    public float getHP(){
+        return HP;
+    }
+    public float getCurrentHP(){
+        return currentHP;
+    }
+    public float getCurrentMP(){
+        return currentMP;
+    }
+    public float getMP() {
+        return MP;
+    }
+    public float getAttackDamage() {
+        return AttackDamage;
+    }
+    public float getArmor() {
+        return Armor;
+    }
+    public float getUltimateCost() {
+        return UltimateCost;
+    }
+    public float getUltimateDamge() {
+        return UltimateDamge;
+    }
+
+    void attack(){
+        System.out.println(AttackMsg);
+    }
+
+    void ultimate(){
+        if(currentMP>=UltimateCost){
+            System.out.println("Ulti ! "+AttackMsg);
+
+        }else{
+            System.out.println("Nub, no mana ecksdee :3 skipping turn...");
+        }
+
+    }
+
+    void ultimatedby(Hero hero){
+        if (hero.currentMP>= hero.UltimateCost){
+            hero.currentMP = hero.currentMP - hero.UltimateCost;
+            this.currentHP = currentHP - ((hero.getUltimateDamge()*5)/(this.Armor+3));
+            if(!this.isAlive()){
+                hero.addmana();
+            }
+        }else{
+            System.out.print("");
+        }
+
+    }
+
+    void getAttackedBy (Hero hero){
+        this.currentHP = currentHP - ((hero.getAttackDamage()*5) / (this.Armor+3));
+        if(!this.isAlive()){
+            hero.addmana();
+        }
+    }
+
+    boolean isAlive(){
+        if (currentHP>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    void addmana (){
+        currentMP += 200;
+    }
+
+}
